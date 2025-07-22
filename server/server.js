c03562f25e08281d01e3871a287afdbbab381bef
mtlha3 <mohdtlha3@gmail.com>
2025-07-22 16:49:28 +0500

    cors solve

--- server/server.js
+++ server/server.js
@@ -13,10 +13,8 @@ dotenv.config();
 const app = express();
 connectDB();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}))
+app.use(cors({ origin: true, credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser())
 
